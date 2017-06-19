fafd166bd312b93cd674399f0a21be01db41a998
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-20 01:10:41 +0200

    Fixed date format

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -32,8 +32,8 @@ class MeetingController extends Controller
         $name        = $request->get('name');
         $description = $request->get('description');
         $user        = User::where('number', $request->get('number'))->first();
-        $starting_at = Carbon::createFromFormat('dd/MM/yy hh:mm', $request->get('starting_at'));
-        $ending_at   = Carbon::createFromFormat('dd/MM/yy hh:mm', $request->get('ending_at'));
+        $starting_at = Carbon::createFromFormat('d/m/y H:i', $request->get('starting_at'));
+        $ending_at   = Carbon::createFromFormat('d/m/y H:i', $request->get('ending_at'));
 
         // Meeting
         $meeting = Meeting::create([
